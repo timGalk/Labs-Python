e7e52c943729fd4af2d18b91286825528cb3f100
timGalk <tivron@outlook.com>
2024-11-28 16:45:57 +0100

    written a logic of shingle.py

--- Lab3/shingle.py
+++ Lab3/shingle.py
@@ -1,6 +1,35 @@
 import argparse as ap
-from Task1 import shingles
 
-def  shingle (args):
-    if len(args) > 1:
-        raise ValueError ("Input is empty")
+from sortedcontainers import SortedSet
+
+from shingles import shingles
+
+def counter (arr: list) -> dict:
+
+    # counter has a struclure {key: value - list[str]}
+    #very specific thing : if two keys has the same value
+
+    unique_phrases_set = set(arr)
+    counter = {}
+
+    for combination in unique_phrases_set:
+        if combination not in counter.keys():
+            counter[combination] = 1
+        else:
+            counter[combination] += 1
+
+     # the counter of the most popular phrase
+    the_most_popular_phrase = max(counter.values())
+    result = set()
+    for key in counter.keys():
+        if counter[key] == the_most_popular_phrase:
+            result.add(key)
+
+    return result
+
+
+def main():
+    lst = shingles("one two three four five ", 3)
+    print(', ')
+
+if __name__ == "__main__": main()
